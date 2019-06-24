d725c1b530496a316977c897bf4bab479d728fb7
Andres <43399076+cotoa412@users.noreply.github.com>
2019-06-24 16:50:46 -0600

    Add files via upload

---
+++ SalesQuest/src/java/com/salesquest/login/LoginController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.salesquest.login;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Kainthel
+ */
+
+@ManagedBean(name = "loginController")
+@SessionScoped
+public class LoginController {
+
+    private String usuario;
+    private String clave;
+    private String mensaje;
+
+    public LoginController() {
+        this.mensaje = "Bienvenidos a SalesQuest";
+    }
+
+    public LoginController(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public void ingresar() {
+        System.out.println("El valor digitado usuario es: " + this.getUsuario());
+        System.out.println("El valor digitado Clave es: " + this.getClave());
+        System.out.println("El valor digitado Mensaje es: " + this.getMensaje());
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (this.getUsuario().equalsIgnoreCase("admin") && (this.getClave().equalsIgnoreCase("testtest"))) { //Usuario registrado
+
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", this.getUsuario()));
+
+        } else {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No es admin"));
+        }
+        
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+}
